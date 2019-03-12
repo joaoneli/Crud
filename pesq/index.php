687d30453f4a723e12a71b1cd2b0a75c06c6f1e7
joaoneli <39301199+joaoneli@users.noreply.github.com>
2019-03-12 15:18:00 -0300

    Add files via upload

---
+++ pesq/index.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+include_once("conexao.php");
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>CRUD - Barao</title>		
+	</head>
+	<body>
+		<a href="cad_usuario.php">Cadastrar</a><br>
+		<a href="index.php">Listar</a><br>
+		<a href="pesquisar.php">Pesquisar</a><br>
+		<h1>Listar Usuário</h1>
+		<?php
+		if(isset($_SESSION['msg'])){
+			echo $_SESSION['msg'];
+			unset($_SESSION['msg']);
+		}
+	
+		$resulta_usuarios = "SELECT * FROM usuarios ";
+		$resultado_usuarios = mysqli_query($conn, $resulta_usuarios);
+		while($row_usuario = mysqli_fetch_assoc($resultado_usuarios)){
+			echo "ID: " . $row_usuario['id'] . "<br>";
+			echo "Nome: " . $row_usuario['nome'] . "<br>";
+			echo "E-mail: " . $row_usuario['email'] . "<br>";
+			echo "<a href='edit_usuario.php?id=" . $row_usuario['id'] . "'>Editar</a><br>";
+			echo "<a href='proc_apagar_usuario.php?id=" . $row_usuario['id'] . "'>Apagar</a><br><hr>";
+		}
+		
+		?>		
+	</body>
+</html>
